6757c4239b9985022764ed7b1bf5daa42ef636a4
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-06-27 22:32:16 -0300

    Documentation

---
+++ src/QN8066.cpp
@@ -0,0 +1,14 @@
+/**
+ * @mainpage QN8066 Arduino Library implementation
+ *
+ * Contact: pu2clr@gmail.com
+ *
+ * @details QN8066
+ *
+ * @author PU2CLR - Ricardo Lima Caratti
+ * @date  2024-06
+ * @copyright MIT Free Software model. See [Copyright (c) 2019 Ricardo Lima Caratti](https://pu2clr.github.io/QN8066/#mit-license).
+ */
+
+#include <QN8066.h>
+
---
+++ src/QN8066.h
@@ -0,0 +1,20 @@
+/**
+ * @brief QN8066 ARDUINO LIBRARY
+ *
+ *
+ * @author PU2CLR - Ricardo Lima Caratti
+ * @date  2024-06
+ */
+
+#ifndef _QN8066_H // Prevent this file from being compiled more than once
+#define _QN8066_H
+
+#include <Arduino.h>
+#include <Wire.h>
+
+
+
+
+
+
+#endif // _QN8066_H
