4a8ad232bc3985c593ca33c72f679950ddc58836
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-07-19 17:43:41 -0300

    Doc

--- examples/01_SERIAL_MONITOR/A_MINIMALIST_TX/A_MINIMALIST_TX.ino
+++ examples/01_SERIAL_MONITOR/A_MINIMALIST_TX/A_MINIMALIST_TX.ino
@@ -17,29 +17,16 @@ the Arduino Uno, Nano, or Pro Mini boards.
 
 Attention: Use the "Serial Monitor" function in the Arduino IDE to monitor the system 
            initialization process.
+
 Author: Ricardo Lima Caratti (PU2CLR) - 2024/06/14
 */
+
 #include <QN8066.h>
 
 #define PWM_PIN   9      // Arduino PIN used to control the output power of the transmitter via PWM.
-#define FREQUENCY 1069   // 106.9 MHz 
+#define FREQUENCY 1069   // 106.9 MHz - This library does not use floating-point data. 
+                         // This approach helps to save microcontroller memory. 
+                         // Therefore, to represent a frequency in the commercial FM band, 
+                         // multiply the desired frequency by 10. In this case 109.7MHz is 1069.
 
 QN8066 tx;
-void setup() {
-  Serial.begin(9600);
-  while (!Serial) ;
-  pinMode(PWM_PIN, OUTPUT); // Sets the Arduino PIN to operate with with PWM
-  if (!tx.detectDevice()) {
-    Serial.println("\nDevice QN8066 not detected");
-    while (1);
-  }
-  delay(500); // Wait a bit while the system stabilizes.
-  tx.setup();
-  Serial.print("\nStarting...");
-  tx.setTX(FREQUENCY);    // Chenge the FREQUENCY constant if you want other value
-  Serial.print("\nBroadcasting...");
-  analogWrite(9, 50);  // Power controller via PWM - It is about 1/5 of the max power. It is between 1 and 1,4 W
-}
-
-void loop() {
-}
