cb5f168d4484d5d1ec4fe2767b9addf554a03f48
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-08-13 14:16:51 -0300

    Doc

--- examples/03_LCD_16x2_AND_20x4/STANDALONE_ATMEGA328_ENCODER/STANDALONE_ATMEGA328_ENCODER.ino
+++ examples/03_LCD_16x2_AND_20x4/STANDALONE_ATMEGA328_ENCODER/STANDALONE_ATMEGA328_ENCODER.ino
@@ -57,37 +57,31 @@
   |                           | SCLK                      |     A5        |
   |                           | PWM                       |     D9        |
   | --------------------------| --------------------------| --------------|
-  | Buttons                   |                           |               |
+  | Button                    |                           |               |
   |                           | Menu                      |     A0/D14    |
-  |                           | Left (Down / -)           |     10        |
-  |                           | Right (Up / + )           |     11        |
+  | Encoder                   |                           |               |
+  |                           | Left                      |     D2        |
+  |                           | Right                     |     D3        |
   | --------------------------| --------------------------|---------------|
 
   IMPORTANT: 
-  There are some important considerations when using a microcontroller like the 
-  Arduino Nano with the "DIY 5~7W FM Transmitter Kit". This kit is designed to 
-  work with a 3.3V controller, meaning the I2C bus is configured with pull-up 
-  resistors connected to the board's power supply. However, the Arduino Nano 
-  operates at 5V, producing a 5V signal on the I2C bus, which can cause 
-  instability in I2C communication. Additionally, the Arduino Nano will not 
-  operate stably when powered by a 3.3V supply (provided by the kit).
 
-  The most straightforward and suitable solution, though not very practical, would 
-  be to modify the Arduino Nano to operate at 3.3V (by changing the crystal to 8MHz,
-  among other modifications, including changing the bootloader). The most viable 
-  solution would be to use an "Arduino Pro Mini 3.3V". This way, no changes or 
-  adaptations would be necessary to connect the kit to the microcontroller.
+  Usage and Operation Instructions:
 
-  For the Arduino Nano, the adaptation used in this example involves using a 3.3V to 
-  5V voltage converter to power the Arduino and placing two 150-ohm resistors in series 
-  with the I2C bus. Although this is not best practice, it works to some extent. 
-  If you insist in using a Arduino Nano, a more appropriate solution would be to use a 
-  bidirectional I2C level shifter to make the I2C communication compatible with both 
-  the Arduino Nano and the kit**.
+  1) To access the MENU, quickly press the knob or rotary shaft of the encoder down. 
+  2) Select the menu item you wish to adjust by turning the encoder clockwise or counterclockwise. 
+  3) Once you've chosen the item you want to change, press the encoder again. 
+  4) Turn the encoder clockwise or counterclockwise to select the new parameters. 
+  5) Finally, press the encoder one more time to confirm and apply the parameter change.
 
-  Finally, the Arduino Nano was used in this example because it is widely used by most 
-  hobbyists. It's likely that many already have one at home, or even an Arduino Uno,
-  which shares the same architecture.
+  It is important to highlight that if RDS is activated/enabled, it will be temporarily disabled while
+  the user interacts with the MENU and parameter adjustments. Once the adjustments are complete, the 
+  RDS will resume functioning after a few seconds. This occurs to improve the system's responsiveness, 
+  as RDS processing requires synchronization and a processing load that can, to some extent, impact the
+  user interface.
+
+
+
 
   Prototype documentation: https://pu2clr.github.io/QN8066/
   PU2CLR QN8066 API documentation: https://pu2clr.github.io/QN8066/extras/apidoc/html/
