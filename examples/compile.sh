11b3f72acc76b1679124b635a79f30d6d38824a3
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-08-15 15:50:36 -0300

    doc

--- examples/compile.sh
+++ examples/compile.sh
@@ -22,6 +22,15 @@ arduino-cli compile -b arduino:avr:nano ./02_RDS/TX_RDS2 --output-dir ~/Download
 echo "---> NANO_QN8066_LCD16x02_V3"   
 arduino-cli compile -b arduino:avr:nano ./03_LCD_16x2_AND_20x4/01_ARDUINO/NANO_PRO_MINI_3_BUTTONS_LCD16x2 --output-dir ~/Downloads/hex/atmega/LCD16x2_arduino
 
+echo "MiniCore - It can help you to save memory on ATmega328"
+arduino-cli compile -b MiniCore:avr:328:bootloader=no_bootloader,LTO=Os_flto  ./03_LCD_16x2_AND_20x4/02_ATMEGA328/STANDALONE_ATMEGA328_ENCODER --output-dir ~/Downloads/hex/MiniCore/03_LCD_16x2_AND_20x4
+
+echo "ESP32"
+echo "ESP32 LCD16x2 AND ENCODER"
+arduino-cli compile --fqbn esp32:esp32:esp32-poe-iso ./03_LCD_16x2_AND_20x4/03_ESP32/ESP32_ENCODER --output-dir ~/Downloads/hex/ESP32/DEVM/LCD16x2_encoder
+
+
+
 echo "ATTINY85"
 arduino-cli compile --fqbn ATTinyCore:avr:attinyx5:clock=1internal ./06_ATTINY85_RDS_OLED --output-dir ~/Downloads/hex/ATTIMY85/ATTINY85
 
