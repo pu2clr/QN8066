9859e3d9d3c3651ce66b1240a8d866f797d42753
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-08-27 18:24:30 -0300

    Doc

--- src/QN8066.cpp
+++ src/QN8066.cpp
@@ -1897,7 +1897,7 @@ void QN8066::rdsSendDateTime(uint16_t year, uint8_t month, uint8_t day, uint8_t 
 
   // RDS_DATE_TIME rdsDateTime;
 
-  uint16_t mjd = (1461 * (year + 4800 + (month - 14) / 12)) / 4 +
+  uint32_t mjd = (1461 * (year + 4800 + (month - 14) / 12)) / 4 +
            (367 * (month - 2 - 12 * ((month - 14) / 12))) / 12 -
            (3 * ((year + 4900 + (month - 14) / 12) / 100)) / 4 + day - 32075 - 2400001;
 
