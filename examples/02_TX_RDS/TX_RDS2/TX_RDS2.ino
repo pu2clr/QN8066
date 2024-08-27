1b2c076748385b1116e3a71657e05842da837bd5
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-08-27 18:29:04 -0300

    Doc

--- examples/02_TX_RDS/TX_RDS2/TX_RDS2.ino
+++ examples/02_TX_RDS/TX_RDS2/TX_RDS2.ino
@@ -5,6 +5,7 @@ QN8066 tx;
 void setup() {
  tx.setup(1000,false,true);
  tx.setTX(1069);   // Sets frequency to 106.9 MHz 
+ tx.rdsEnableRX(true);
  tx.rdsInitTx(0x8,0x1,0x9B, 8, 50, 20);// Sets PI code, sync delay and more     
 }
 void loop() {
