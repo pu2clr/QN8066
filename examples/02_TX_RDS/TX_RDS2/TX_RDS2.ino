dce7a503414a78e0879b665b1e056e22f721b13e
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-08-27 18:49:27 -0300

    Doc

--- examples/02_TX_RDS/TX_RDS2/TX_RDS2.ino
+++ examples/02_TX_RDS/TX_RDS2/TX_RDS2.ino
@@ -11,6 +11,6 @@ void setup() {
 void loop() {
     tx.rdsSendPS(ps);
     tx.rdsSendRTMessage(rt);
-    tx.rdsSendDateTime(2024, 8, 26, 23, 20, -3);
+    // tx.rdsSendDateTime(2024, 8, 26, 23, 20, -3);
     delay(3000);    
 }
